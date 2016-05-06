18e70496d6eef32c10b1aa18ce9786eb203a2656
Quanfu <qf.huo@outlook.com>
2016-05-06 10:07:30 +0800

    Add distinct List value

---
+++ src/test/java/Collections/CollectionsUnitTest.java
@@ -0,0 +1,15 @@
+package Collections;
+
+import org.junit.Test;
+
+/**
+ * Created by huoquanfu on 2016/5/5.
+ */
+public class CollectionsUnitTest {
+    @Test
+    public void Test() {
+        CollectionsTest.distinctByHashSet();
+
+        CollectionsTest.distinctByLamada();
+    }
+}
